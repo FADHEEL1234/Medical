f91ed92650d16808238782616cd2a2e2c1eb730d
FADHEEL1234 <fadhilmohamed512@gmail.com>
2026-02-27 14:57:05 +0300

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        env='DATABASE_URL',
         conn_max_age=600,
-        ssl_require=not DEBUG,
+        ssl_require=True,
     )
 }
 
